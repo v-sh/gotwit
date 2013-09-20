e46d7bdd6acc27ececeea753d20bb04313877abf
v.shushlin <vladimirshushlin@mail.ru>
2013-09-20 12:29:31 +0400

    decompisition

--- front/front.go
+++ front/front.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
 	"log"
 	"code.google.com/p/goconf/conf"
 	"flag"
-	"path/filepath"
+	"github.com/v-sh/gotwit/front/httpServe"
 )
 
-var allTmpl template.Template
-var mainTmpl *template.Template//= template.Must(template.New("mainPage").Parse(mainPage))
-//var _, _ = templ.Parse(innerDiv)
 var confFileName string
 var mainConf *conf.ConfigFile
 
@@ -20,65 +15,16 @@ func main() {
 	var err error
 	mainConf, err = conf.ReadConfigFile(confFileName)
 	
-	initTemplates()
-	startServer()
+	httpServe.Run(mainConf)
 	if err != nil {
 		log.Panicf("Error reading config file: err %s", err);
 	}
-}
-
-func initTemplates() {
-	tmplDir, err := mainConf.GetString("default", "tmpl_dir");
-	if err != nil {
-		log.Panicf("not specified tmpl_dir: %s", err)
-	}
-	tmplPattern, err := mainConf.GetString("default", "tmpl_pattern")
-	if err != nil {
-		tmplPattern = "*.tmpl"
-	}
-
-	tmplFiles := filepath.Join(tmplDir, tmplPattern)
-	allTmpl, err := template.ParseGlob(tmplFiles)
-	if err != nil {
-		log.Panicf("err parse template:%s", err)
-	}
-	mainPageTmplName, err := mainConf.GetString("default", "tmpl_mainpage")
-	if err != nil {
-		log.Panicf("not specified tmpl_mainpage: %s",err)
-	}
-
-	mainTmpl = allTmpl.Lookup(mainPageTmplName)
-	if mainTmpl == nil {
-		log.Panicf("there is no template '%s' in parsed templates", mainPageTmplName)
-	}
-}
-
-func startServer(){
-	http.Handle("/", http.HandlerFunc(rootHandler))
-	host, err := mainConf.GetString("default", "host")
-	if err != nil {
-		log.Panic("not specified host");
-	}
-	port, _ := mainConf.GetString("default", "port")
-	if err != nil {
-		log.Panic("not specified port");
-	}
-	err = http.ListenAndServe(host + ":"+  port, nil)
-	if err != nil {
-		log.Fatal("ListernAndServe:", err)
-	}
-	
 }
 
 func init() {
 	flag.StringVar(&confFileName, "c", "/usr/local/etc/gotwit/front.conf", 
 		"config file");
 }
-
-func rootHandler(w http.ResponseWriter, req *http.Request) {
-	mainTmpl.Execute(w, req.FormValue("s"));
-}
-
 
 
 
---
+++ front/httpServe/httpServe.go
@@ -0,0 +1,66 @@
+package httpServe
+
+import (
+	"html/template"
+	"net/http"
+	"log"
+	"code.google.com/p/goconf/conf"
+	"path/filepath"
+)
+
+var allTmpl template.Template
+var mainTmpl *template.Template
+
+
+func Run(conf *conf.ConfigFile) {
+	initTemplates(conf)
+	startServer(conf)
+}
+
+func initTemplates(conf *conf.ConfigFile) {
+	tmplDir, err := conf.GetString("default", "tmpl_dir");
+	if err != nil {
+		log.Panicf("not specified tmpl_dir: %s", err)
+	}
+	tmplPattern, err := conf.GetString("default", "tmpl_pattern")
+	if err != nil {
+		tmplPattern = "*.tmpl"
+	}
+
+	tmplFiles := filepath.Join(tmplDir, tmplPattern)
+	allTmpl, err := template.ParseGlob(tmplFiles)
+	if err != nil {
+		log.Panicf("err parse template:%s", err)
+	}
+	mainPageTmplName, err := conf.GetString("default", "tmpl_mainpage")
+	if err != nil {
+		log.Panicf("not specified tmpl_mainpage: %s",err)
+	}
+
+	mainTmpl = allTmpl.Lookup(mainPageTmplName)
+	if mainTmpl == nil {
+		log.Panicf("there is no template '%s' in parsed templates", mainPageTmplName)
+	}
+}
+
+
+func startServer(conf *conf.ConfigFile){
+	http.Handle("/", http.HandlerFunc(rootHandler))
+	host, err := conf.GetString("default", "host")
+	if err != nil {
+		log.Panic("not specified host");
+	}
+	port, _ := conf.GetString("default", "port")
+	if err != nil {
+		log.Panic("not specified port");
+	}
+	err = http.ListenAndServe(host + ":"+  port, nil)
+	if err != nil {
+		log.Fatal("ListernAndServe:", err)
+	}
+	
+}
+
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+	mainTmpl.Execute(w, req.FormValue("s"));
+}
