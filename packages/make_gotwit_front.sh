9fef27bb5b7b353f33dda25781819e7c21482b1b
Ubuntu <ubuntu@ip-10-0-0-195.eu-west-1.compute.internal>
2014-01-05 18:06:55 +0000

    some work on packages

--- packages/make_gotwit_front.sh
+++ packages/make_gotwit_front.sh
@@ -2,18 +2,25 @@ FRONT_SRC_DIR=../front
 PACKAGE_DIR=gotwit-front
 VERSION=`date +"%Y.%m.%d.%H.%M.%S"`
 CONTROL_FILE=$PACKAGE_DIR/DEBIAN/control
+PACKAGE_FULL_NAME=$PACKAGE_DIR"_"$VERSION"_amd64.deb"
+BIN_DIR=$PACKAGE_DIR/usr/bin
 
+rm -r $PACKAGE_DIR/usr
+mkdir -p $BIN_DIR
 
 echo "Version="$VERSION
 
 (cd $FRONT_SRC_DIR; go build) &&
-cp $FRONT_SRC_DIR/front $PACKAGE_DIR/usr/local/bin/front_d &&
+cp $FRONT_SRC_DIR/front $BIN_DIR/front_d &&
 
 
-(cd $PACKAGE_DIR; md5deep -r usr > DEBIAN/md5sums) &&
+(cd $PACKAGE_DIR; md5deep -l -r usr > DEBIAN/md5sums) &&
 
-rm $PACKAGE_DIR"*.deb"
+rm $PACKAGE_DIR*.deb
+chmod -R g-w $PACKAGE_DIR
 sed 's/Version:.*/Version: '$VERSION'/' -i $CONTROL_FILE &&
 fakeroot dpkg-deb --build $PACKAGE_DIR &&
-mv $PACKAGE_DIR.deb $PACKAGE_DIR"_"$VERSION"_amd64.deb"
+mv $PACKAGE_DIR.deb $PACKAGE_FULL_NAME
+
+lintian $PACKAGE_FULL_NAME
 
