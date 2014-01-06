7c17e8311168814bed5b0c8543a06b8e38bc86e2
v.shushlin <VladimirShushlin@mail.ru>
2014-01-06 11:31:26 +0000

    some work on packages and repo

--- packages/make_gotwit_front.sh
+++ packages/make_gotwit_front.sh
@@ -4,6 +4,7 @@ VERSION=`date +"%Y.%m.%d.%H.%M.%S"`
 CONTROL_FILE=$PACKAGE_DIR/DEBIAN/control
 PACKAGE_FULL_NAME=$PACKAGE_DIR"_"$VERSION"_amd64.deb"
 BIN_DIR=$PACKAGE_DIR/usr/bin
+REPO_DIR=$HOME/rep_apt
 
 rm -r $PACKAGE_DIR/usr
 mkdir -p $BIN_DIR
@@ -13,7 +14,7 @@ echo "Version="$VERSION
 (cd $FRONT_SRC_DIR; go build) &&
 cp $FRONT_SRC_DIR/front $BIN_DIR/front_d &&
 
-
+cp control-gotwit-front $CONTROL_FILE
 (cd $PACKAGE_DIR; md5deep -l -r usr > DEBIAN/md5sums) &&
 
 rm $PACKAGE_DIR*.deb
@@ -24,3 +25,7 @@ mv $PACKAGE_DIR.deb $PACKAGE_FULL_NAME
 
 lintian $PACKAGE_FULL_NAME
 
+(cd $REPO_DIR; rm $PACKAGE_DIR*.deb)
+
+cp *.deb $REPO_DIR &&
+(cd $REPO_DIR; reprepro -C gotwit-soft includedeb gotwit *.deb)
