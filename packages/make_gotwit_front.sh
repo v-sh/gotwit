f3c06e9188002c44f91640b60896a77053e4a4e1
v.shushlin <VladimirShushlin@mail.ru>
2014-01-17 17:14:26 +0000

    another work on packages, nginx diversion, update-rc registration...

--- packages/make_gotwit_front.sh
+++ packages/make_gotwit_front.sh
@@ -3,21 +3,24 @@ PACKAGE_DIR=gotwit-front
 DEBIAN_DIR=$PACKAGE_DIR/DEBIAN
 VERSION=`date +"%Y.%m.%d.%H.%M.%S"`
 CONTROL_FILE=$DEBIAN_DIR/control
+CONFIGFILES_FILE=$DEBIAN_DIR/conffiles
 PACKAGE_FULL_NAME=$PACKAGE_DIR"_"$VERSION"_amd64.deb"
 BIN_DIR=$PACKAGE_DIR/usr/bin
-CONF_DIR=$PACKAGE_DIR/etc/gotwit
+CONF_DIR=$PACKAGE_DIR/etc
 INITS_DIR=../init_scripts
+DEBIAN_SRC_DIR=front_debian_src
 
 CONFS_DIR=../confs
 
 REPO_DIR=$HOME/rep_apt
 
-rm -r $PACKAGE_DIR/usr
-rm -r $PACKAGE_DIR/etc
-rm $DEBIAN_DIR
+rm -rf $PACKAGE_DIR/usr
+rm -rf $PACKAGE_DIR/etc
+rm -r $DEBIAN_DIR
 mkdir -p $BIN_DIR
 mkdir -p $CONF_DIR
 mkdir -p $PACKAGE_DIR/etc/init.d
+mkdir -p $PACKAGE_DIR/usr/share/gotwit/
 mkdir -p $DEBIAN_DIR
 
 echo "Version="$VERSION
@@ -26,13 +29,13 @@ echo "Version="$VERSION
 #build and collect
 (cd $FRONT_SRC_DIR; go build) &&
 cp $FRONT_SRC_DIR/front $BIN_DIR/front_d &&
-cp $CONFS_DIR/front_d.conf $CONF_DIR &&
+cp -R $CONFS_DIR/* $CONF_DIR &&
 cp $INITS_DIR/front_d $PACKAGE_DIR/etc/init.d/ &&
-cp gotwit_front_postinst.sh $DEBIAN_DIR/postinst
+cp -R ../tmpl $PACKAGE_DIR/usr/share/gotwit/ &&
 
 
 #make meta-info
-cp control-gotwit-front $CONTROL_FILE &&
+cp $DEBIAN_SRC_DIR/* $DEBIAN_DIR &&
 sed 's/Version:.*/Version: '$VERSION'/' -i $CONTROL_FILE &&
 (cd $PACKAGE_DIR; md5deep -l -r usr etc > DEBIAN/md5sums) &&
 
@@ -45,7 +48,7 @@ mv $PACKAGE_DIR.deb $PACKAGE_FULL_NAME &&
 
 
 #check package
-lintian $PACKAGE_FULL_NAME &&
+lintian $PACKAGE_FULL_NAME
 
 #update apt-server
 (cd $REPO_DIR; rm $PACKAGE_DIR*.deb) &&
